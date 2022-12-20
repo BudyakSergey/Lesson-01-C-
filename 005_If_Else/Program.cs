a1af0ee14f96fe8dd87532be9c6eb0b3bc857c79
Budyak Sergey <89227322222@mail.ru>
2022-12-20 19:47:35 +0500

    005 If Else

---
+++ 005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите имя пользователя  ");
+string userName = Console.ReadLine ();
+if (userName.ToLower() == "маша")
+{
+    Console.Write ("Ура! Это же МАША!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.Write (userName);
+}
