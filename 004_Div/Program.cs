96535c4c458fde5a3e1fde44272284fc99c2e0bc
Budyak Sergey <89227322222@mail.ru>
2022-12-20 19:29:38 +0500

    004 Div

---
+++ 004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = 12;
+int numberB = 5;
+double result = numberA / numberB;
+// int result = numberA + numberB;
+// Console.WriteLine (numberA / numberB);
+Console.WriteLine (result);
