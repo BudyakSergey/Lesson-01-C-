223fb71e076aac33f55879dc3ad28837b785780a
Budyak Sergey <89227322222@mail.ru>
2022-12-22 16:52:55 +0500

    end lesson2

---
+++ 011_ArrayLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] coll)
+{
+    int len = coll.Length;
+    int index2 = 0;
+    while (index2 < len)
+    {
+        coll[index2] = new Random().Next(1, 10);
+        index2 = index2 + 1;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position = position + 1;
+    }
+}
+int[] array = new int[10]; // в массиве 10 элементов
+FillArray(array);
+PrintArray(array);  
